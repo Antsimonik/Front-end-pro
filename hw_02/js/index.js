ab8ed92aa435141338bb099bd5dc7aa93aab5005
Alex <antimonikalexandr@gmail.com>
2022-07-08 18:11:43 +0200

    fixed year and prompt

--- hw_02/js/index.js
+++ hw_02/js/index.js
@@ -1,10 +1,10 @@
-let ageYear = "1988";
+const AGE_YEAR = 1988;
 let firstName = "John";
 let lastName = "Doe";
-const currentYear = 2022;
-let age = currentYear - ageYear;
+const CURRENT_YEAR = 2022;
+let age = CURRENT_YEAR - AGE_YEAR;
 
-prompt ("Year of birth?", Number(ageYear));
+prompt ("Year of birth?", AGE_YEAR);
 prompt ("Your First Name?", firstName);
 prompt ("Your Last Name?", lastName);
 console.log("'User Bio:", firstName, lastName + ",",  age, "years old;'")
