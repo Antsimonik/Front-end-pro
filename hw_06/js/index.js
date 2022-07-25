a190d9e19b3a18b8e22e8c9a2370e88b9575f3f3
Alex <antimonikalexandr@gmail.com>
2022-07-25 21:34:30 +0200

    edited

--- hw_06/js/index.js
+++ hw_06/js/index.js
@@ -11,115 +11,68 @@ const IS_HAMBURGER = `hamburger`;
       ANSWER_YES = `yes`;
 
 
-let stringHasNumber = false;
-    currentCash = 0;
-    isTrueMenu = true;
+let currentCash = 0;
     userMenu = String();
-    userMenuPotato = String();
-    userMenuSauce = String();
-    userMenuCheeseburger = String();
-
-
+    isTrueMenu = true;
 
 do {
   userMenu = prompt(`${IS_HAMBURGER} or ${IS_CHEESEBURGER}?`, `cheeseburger`);
-  isTrueMenu = userMenu === IS_HAMBURGER || userMenu === IS_CHEESEBURGER;
-  if(isTrueMenu) {
-    userMenu = userMenu.trim().toLowerCase();
-    if (userMenu === IS_HAMBURGER){ 
-      currentCash += HAMBURGER_CASH;
-    } else if (userMenu === IS_CHEESEBURGER) {
-      currentCash += CHEESEBURGER_CASH;
-      userMenuCheeseburger = prompt(`Would you like to add double cheese?`, `yes`).trim().toLowerCase();
-      if (userMenuCheeseburger === ANSWER_YES) {
-          currentCash += DOUBLE_CHEESE;
-      }
-    } 
-  }
-} while (!isTrueMenu){
+  if (isTrueMenu) userMenu = userMenu.trim().toLowerCase();
+} while (userMenu !== IS_HAMBURGER && userMenu !== IS_CHEESEBURGER);
+
+switch(userMenu){
+  case IS_HAMBURGER:
+    currentCash += HAMBURGER_CASH;
+    break;
+  case IS_CHEESEBURGER:
+    currentCash += CHEESEBURGER_CASH;
+    let userMenuCheeseburger = confirm(`Would you like to add double cheese?`);
+    if(userMenuCheeseburger) currentCash += DOUBLE_CHEESE;
 }
 
 console.log(`${currentCash} $, ${isTrueMenu}, ${userMenu}`)
 
-userMenuPotato = prompt(`Would you like potato?`, 'yes');
-userMenuPotato = userMenuPotato.trim().toLowerCase();
-if (userMenuPotato === ANSWER_YES){
+let userMenuPotato = confirm(`Would you like potato?`);
+if (userMenuPotato){
   userMenuPotato = prompt(`Choose potato size: small/middle/big`, 'small');
-  userMenuPotato = userMenuPotato.trim().toLowerCase();
-  if (!userMenuPotato || userMenuPotato === `small` || userMenuPotato === ` ` || userMenuPotato === ""){
-    currentCash += SMALL_POTATO;
-  } else if (userMenuPotato === `middle`){
-        currentCash += MIDDLE_POTATO;
-  } else if (userMenuPotato === `big`){
-        currentCash += BIG_POTATO;
+  if(userMenuPotato) userMenuPotato = userMenuPotato.trim().toLowerCase();
+
+  switch(userMenuPotato){
+    case `middle`:
+      currentCash += MIDDLE_POTATO;
+      break;
+    case `big`:
+      currentCash += BIG_POTATO;
+      break;
+    default:
+      currentCash += SMALL_POTATO;
+      userMenuPotato = `small`;
   }
 }
 console.log(`${currentCash} $, ${userMenuPotato}`)
 
-userMenuSauce = prompt(`Would you like sauce?`, `yes`);
-userMenuSauce = userMenuSauce.trim().toLowerCase();
-if (userMenuSauce === ANSWER_YES) {
+let userMenuSauce = confirm(`Would you like sauce?`, `yes`);
+if (userMenuSauce) {
   userMenuSauce = prompt(`Choose sauce: ketchup/mayonnaise`, `ketchup`);
-  userMenuSauce = userMenuSauce.trim().toLowerCase();
-  if (!userMenuSauce || userMenuSauce === `ketchup` || userMenuSauce === " " || userMenuSauce === "") {
-    currentCash += KETCHUP;
-  } else if (userMenuSauce === `mayonnaise`) {
-    currentCash += MAYONNAISE;
+  if (userMenuSauce) userMenuSauce = userMenuSauce.trim().toLowerCase();
+
+  switch(userMenuSauce) {
+    case `mayonnaise` :
+      currentCash += MAYONNAISE;
+      break;
+    default:
+      currentCash += KETCHUP;
+      userMenuSauce = `ketchup`
   }
   console.log(`${currentCash}$, ${userMenu}, ${userMenuPotato}, ${userMenuSauce}`)
 }
 
-if ((userMenuPotato && userMenuSauce) || (userMenuPotato&& userMenuSauce)) {
-  document.write(`
-<div class="price" id="Allprice">
-  <h2>Your order:</h2>
-<ul>
-  <li class="Bulka">Bulka 🍔: ${userMenu}
-  </li>
-  <li class="Potato">Potato 🍟: ${userMenuPotato}
-  </li>
-  <li class="Sauce">Sauce 🧂: ${userMenuSauce}
-  </li>
-</ul>
-
-<p>Price: ${currentCash}$ </p>  
-</div>`)
-} else if (userMenuPotato && !userMenuSauce) {
-  document.write(`
-<div class="price" id="priceBulkaPotato">
-  <h2>Your order:</h2>
-<ul>
-  <li class="Bulka">Bulka 🍔: ${userMenu}
-  </li>
-  <li class="Potato">Potato 🍟: ${userMenuPotato}
-  </li>
-</ul>
-
-<p>Price: ${currentCash}$ </p>  
-</div>`)
-} else if (userMenuSauce && userMenuSauce && !userMenuPotato) {
-  document.write(`
-<div class="price" id="priceBulkaSauce">
-  <h2>Your order:</h2>
-<ul>
-  <li class="Bulka">Bulka 🍔: ${userMenu}
-  </li>
-  <li class="Sauce">Sauce 🧂: ${userMenuSauce}
-  </li>
-</ul>
-
-<p>Price: ${currentCash}$ </p>  
-</div>`)
-} else {
-  document.write(`
-<div class="price" id="priceBulka">
-  <h2>Your order:</h2>
-<ul>
-  <li class="Bulka">Bulka 🍔: ${userMenu}
-  </li>
-</ul>
-
-<p>Price: ${currentCash}$ </p>  
-</div>`);
-}
+document.write(`<h2>Your order is:</h2>
+    <ol>
+        <li>🍔Bun is: ${userMenu};</li>
+        ${userMenuPotato ? `<li>🍟Potato size is: ${userMenuPotato};</li>` : ``}
+        ${userMenuSauce ? `<li>🧂Sauce is: ${userMenuSauce};</li>` : ``}
+    </ol>
+    <p>Price is: ${currentCash}.</p>
+`);
 
