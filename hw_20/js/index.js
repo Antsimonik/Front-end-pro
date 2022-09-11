07811cc5355ceff459bca89aa23740a79d47619e
Alex <antimonikalexandr@gmail.com>
2022-09-11 14:37:55 +0200

    hw_20. Shapes ❤️🟢🔷

--- hw_20/js/index.js
+++ hw_20/js/index.js
@@ -1,35 +1,13 @@
 const checkedFigureClass = document.querySelector(`#figureClass`);
-const figureRender = document.querySelector(`#figureRender`);
+const figureColors = document.querySelector(`input[name="figureColor"]`);
 const element = document.querySelector(`#element`);
-const figureForm = document.querySelector(`#figureForm`)
+const figureForm = document.querySelector(`#figureForm`);
 
 figureForm.addEventListener(`submit`, event => {
 
-  event.preventDefault();
+	event.preventDefault();
 
-  let variant = checkedFigureClass.value;
-  let colors = [...document.querySelectorAll(`input[name="figureColor"]`)].map(item => item.value);
-
-  let figure = {
-      variant: variant,
-      colors: colors
-  }
-
-  if(figure.colors) {
-    element.style.background = figure.colors;
-  }
-
-  switch (figure.variant) {
-    case `square`:
-      element.classList.add(`square`);
-      break;
-    case `rectangle`:
-      element.classList.add(`rectangle`);
-      break;
-    case `circle`:
-      element.classList.add(`circle`);
-      break;  
-  };
-  console.log(figure);
-  console.log(JSON.stringify(figure));
-})
+	element.style.background = figureColors.value;
+	element.classList = checkedFigureClass.value;
+  console.log(figureRender.value, checkedFigureClass.value)
+});
