4188655bceb7536c26b442b1dee389acee339292
Alex <antimonikalexandr@gmail.com>
2024-05-02 17:42:51 +0200

    create ProductDisplay

--- ecommerce_web/src/Components/ProductDisplay/ProductDisplay.jsx
+++ ecommerce_web/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -25,7 +25,7 @@ function ProductDisplay(props) {
       </div>
       <div className="product-display-right">
         <h1>{product.name}</h1>
-        <div className="product-display-right-star">
+        <div className="product-display-right-stars">
           <img src={star_icon} alt="star_icon" />
           <img src={star_icon} alt="star_icon" />
           <img src={star_icon} alt="star_icon" />
@@ -33,6 +33,34 @@ function ProductDisplay(props) {
           <img src={star_dull_icon} alt="star_dull_icon" />
           <p>(122)</p>
         </div>
+        <div className="product-display-right-prices">
+          <div className="product-display-right-prices-old">
+            ${product.old_price}
+          </div>
+          <div className="product-display-right-prices-new">
+            ${product.new_price}
+          </div>
+        </div>
+        <div className="product-display-right-description">
+          <p>A lightweigh, usually knitted, pullover shirt, close-fitting...</p>
+        </div>
+        <div className="product-display-right-size">
+          <h2>Select Size</h2>
+          <div className="product-display-right-size-all">
+            <div>S</div>
+            <div>M</div>
+            <div>L</div>
+            <div>XL</div>
+            <div>XXL</div>
+          </div>
+        </div>
+        <button>ADD TO CART</button>
+        <p className="product-display-right-category">
+          <span>Category :</span> Women, T-Shirt, Crop Top
+        </p>
+        <p className="product-display-right-category">
+          <span>Tags :</span> Modern, Latest
+        </p>
       </div>
     </div>
   );
--- ecommerce_web/src/Pages/Product.jsx
+++ ecommerce_web/src/Pages/Product.jsx
@@ -4,7 +4,7 @@ import { useParams } from "react-router-dom";
 import Breadcrum from "../Components/Breadcrums/Breadcrum";
 import ProductDisplay from "../Components/ProductDisplay/ProductDisplay";
 
-function Product(props) {
+function Product() {
   const { all_product } = useContext(ShopContext);
   const { productId } = useParams();
   const product = all_product.find((e) => e.id === Number(productId));
