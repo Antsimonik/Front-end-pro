3fd898246f0326e915fa71363ab7001928a135ab
Alex <antimonikalexandr@gmail.com>
2024-06-28 21:33:31 +0200

    localStorage auth-token,  add, remove

--- E-Commerce_React/backend/index.js
+++ E-Commerce_React/backend/index.js
@@ -236,12 +236,39 @@ const fetchUser = async (req, res, next) => {
 
 // ? creating endpoint for adding products in cartdata
 app.post("/addtocart", fetchUser, async (req, res) => {
-  console.log(req.body, req.user);
+  console.log("Added", req.body.itemId);
+  let userData = await Users.findOne({ _id: req.user.id });
+  userData.cartData[req.body.itemId] += 1;
+  await Users.findOneAndUpdate(
+    { _id: req.user.id },
+    { cartData: userData.cartData }
+  );
+  res.send("Added");
+});
+
+// creating endpoint to remove product from cartdata
+app.post("/removefromcart", fetchUser, async (req, res) => {
+  console.log("Removed", req.body.itemId);
+  let userData = await Users.findOne({ _id: req.user.id });
+  if (userData.cartData[req.body.itemId] > 0)
+    userData.cartData[req.body.itemId] -= 1;
+  await Users.findOneAndUpdate(
+    { _id: req.user.id },
+    { cartData: userData.cartData }
+  );
+  res.send("Removed");
+});
+
+// creating endpoint to get cartdata
+app.post("/getcart", fetchUser, async (req, res) => {
+  console.log("GetCart");
+  let userData = await Users.findOne({ _id: req.user.id });
+  res.json(userData.cartData);
 });
 
 app.listen(port, (error) => {
   if (!error) {
-    console.log("Server Running on Port" + port);
+    console.log("Server Running on Port " + port);
   } else {
     console.log("Error : " + error);
   }
