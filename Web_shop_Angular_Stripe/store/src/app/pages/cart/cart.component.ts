95a8eb164c1a1f984da884976e24361a06e891d7
Alex <antimonikalexandr@gmail.com>
2024-08-08 23:03:11 +0200

    complete web-site with angular

--- Web_shop_Angular_Stripe/store/src/app/pages/cart/cart.component.ts
+++ Web_shop_Angular_Stripe/store/src/app/pages/cart/cart.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { loadStripe } from '@stripe/stripe-js';
 import { Cart, CartItem } from 'src/app/models/cart.models';
 import { CartService } from 'src/app/services/cart.service';
 
@@ -32,7 +34,7 @@ displayedColumns: Array<string> = [
   'action'
 ];
 
-  constructor(private cartService: CartService) { }
+  constructor(private cartService: CartService, private http: HttpClient) { }
 
   ngOnInit(): void {
     this.cartService.cart.subscribe((_cart: Cart) => {
@@ -59,6 +61,17 @@ displayedColumns: Array<string> = [
 
   onRemoveQuantity(item: CartItem): void {
     this.cartService.removeQuantity(item);
+  }
+
+  onCheckout(): void {
+    this.http.post('http://localhost:4242/checkout', {
+      items: this.cart.items
+    }).subscribe(async (res: any) => {
+      let stripe = await loadStripe('pk_test_51PkmBIP5bghahzEf4TEXljeLTuKu7kMgCPxHDmy2ACiAmJiJmXtuE4kCeLp4O3UVTkrjFWVMCj4hgeRnKFA3wjRG00v4b6wyXe');
+      stripe?.redirectToCheckout({
+        sessionId: res.id
+      })
+    })
   }
 
 }
