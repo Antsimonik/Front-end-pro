0ce5c0fb803055983535cd09b185e22b19e7e003
Alex <antimonikalexandr@gmail.com>
2024-09-17 17:50:47 +0200

    List of exercises

--- workout_tracker_React_Native_MongoDB/Workouts/App.js
+++ workout_tracker_React_Native_MongoDB/Workouts/App.js
@@ -8,6 +8,8 @@ export default function App() {
     <View style={styles.container}>
       <FlatList
         data={exercises}
+        contentContainerStyle={{ gap: 5 }}
+        keyExtractor={(item, index) => item.name + index}
         renderItem={({ item }) => <ExerciseListItem item={item} />}
       />
 
