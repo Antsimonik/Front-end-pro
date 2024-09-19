e5c415d10aeabcec07aa0eaa1d420124fb4d04dd
Alex <antimonikalexandr@gmail.com>
2024-09-19 22:53:12 +0200

    create new app social media

---
+++ full_stack_social_media_app_react_native/social_media_app/app/welcome.jsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import ScreenWraper from "../components/ScreenWrapper";
+
+const Welcome = () => {
+  return (
+    <ScreenWraper>
+      <Text>Welcome</Text>
+    </ScreenWraper>
+  );
+};
+
+export default Welcome;
+
+const styles = StyleSheet.create({});
