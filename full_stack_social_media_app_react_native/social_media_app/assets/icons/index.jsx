08f6326292f5f48db3506beb92b2c21b594b98a1
Alex <antimonikalexandr@gmail.com>
2024-09-30 18:05:53 +0200

    add icons, edit login page

--- full_stack_social_media_app_react_native/social_media_app/assets/icons/index.jsx
+++ full_stack_social_media_app_react_native/social_media_app/assets/icons/index.jsx
@@ -1,10 +1,50 @@
-import { StyleSheet, Text, View } from "react-native";
+import { View, Text } from "react-native";
 import React from "react";
 import Home from "./Home";
+import Mail from "./Mail";
+import Lock from "./Lock";
+import User from "./User";
+import Heart from "./Heart";
+import Plus from "./Plus";
+import Search from "./Search";
+import Location from "./Location";
+import Call from "./Call";
 import { theme } from "../../constants/theme";
+import Camera from "./Camera";
+import Edit from "./Edit";
+import ArrowLeft from "./ArrowLeft";
+import ThreeDotsCircle from "./ThreeDotsCircle";
+import ThreeDotsHorizontal from "./ThreeDotsHorizontal";
+import Comment from "./Comment";
+import Share from "./Share";
+import Send from "./Send";
+import Delete from "./Delete";
+import Logout from "./logout";
+import Image from "./Image";
+import Video from "./Video";
 
 const icons = {
   home: Home,
+  mail: Mail,
+  lock: Lock,
+  user: User,
+  heart: Heart,
+  plus: Plus,
+  search: Search,
+  location: Location,
+  call: Call,
+  camera: Camera,
+  edit: Edit,
+  arrowLeft: ArrowLeft,
+  threeDotsCircle: ThreeDotsCircle,
+  threeDotsHorizontal: ThreeDotsHorizontal,
+  comment: Comment,
+  share: Share,
+  send: Send,
+  delete: Delete,
+  logout: Logout,
+  image: Image,
+  video: Video,
 };
 
 const Icon = ({ name, ...props }) => {
@@ -21,5 +61,3 @@ const Icon = ({ name, ...props }) => {
 };
 
 export default Icon;
-
-const styles = StyleSheet.create({});
