200b85e76607d6e06d1f1ef840c98b84371105a2
Alex <antimonikalexandr@gmail.com>
2025-01-13 23:55:32 +0100

    create sign in page

--- react_nativ_real_estate/app/(root)/(tabs)/index.tsx
+++ react_nativ_real_estate/app/(root)/(tabs)/index.tsx
@@ -10,7 +10,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text className="font-bold text-lg my-10" >Welcome to estate</Text>
+      <Text className="font-bold my-10 font-rubik text-3xl " >Welcome to Real estate</Text>
       <Link href="/sign-in">Sign In</Link>
       <Link href="/explore">Explore</Link>
       <Link href="/profile">Profile</Link>
