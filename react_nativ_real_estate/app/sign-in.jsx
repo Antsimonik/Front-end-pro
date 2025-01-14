9ef4711a71ce25dd53549d9e5c30be4fa776cb58
Alex <antimonikalexandr@gmail.com>
2025-01-14 23:39:54 +0100

    create oauth google with appwrite

--- react_nativ_real_estate/app/sign-in.jsx
+++ react_nativ_real_estate/app/sign-in.jsx
@@ -30,11 +30,16 @@ const SignIn = () => {
             onPress={handleLogin}
             className="bg-white shadow-md shadow-zinc-300 rounded-full w-full py-4 mt-5"
           >
-            <Image
-              src={icons.google}
-              className="w-5 h-5"
-              resizeMode="contain"
-            />
+            <View className="flex flex-row items-center justify-center">
+              <Image
+                source={icons.google}
+                className="w-5 h-5"
+                resizeMode="contain"
+              />
+              <Text className="text-lg font-rubik-medium text-black-300 ml-2">
+                Continue with Google
+              </Text>
+            </View>
           </TouchableOpacity>
         </View>
       </ScrollView>
