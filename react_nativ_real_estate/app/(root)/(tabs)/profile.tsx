a2db0d321cf332bae82ea52abc17bb99385cb20b
Alex <antimonikalexandr@gmail.com>
2025-01-22 22:30:29 +0100

    create cards

--- react_nativ_real_estate/app/(root)/(tabs)/profile.tsx
+++ react_nativ_real_estate/app/(root)/(tabs)/profile.tsx
@@ -1,7 +1,9 @@
 import { settings } from '@/constants/data';
 import icons from '@/constants/icons';
 import images from '@/constants/images';
-import { View, Text, ScrollView, Image, TouchableOpacity, ImageSourcePropType } from 'react-native'
+import { logout } from '@/lib/appwrite';
+import { useGlobalContext } from '@/lib/global-provider';
+import { View, Text, ScrollView, Image, TouchableOpacity, ImageSourcePropType, Alert } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 interface SettingsItemProps {
@@ -24,7 +26,18 @@ const SettingsItem = ({icon, title, onPress, textStyle, showArrow = true}: Setti
 )
 
 const Profile = () => {
-  const handleLogout = async () => {};  
+  const { user, refetch } = useGlobalContext();
+
+  const handleLogout = async () => {
+    const result = await logout();
+
+    if (result) {
+      Alert.alert("success", "You have been logged out successfully");
+      refetch();
+    } else {
+      Alert.alert("Error", "An error occurred while logging out")
+    }
+  };  
   return (
     <SafeAreaView className='h-full bg-white' >
       <ScrollView showsVerticalScrollIndicator={false}
@@ -35,11 +48,11 @@ const Profile = () => {
         </View>
         <View className="flex-row justify-center flex mt-5">
           <View className="flex flex-col items-center relative mt-5">
-            <Image source={images.avatar} className='size-44 relative rounded-full' />
+            <Image source={{uri: user?.avatar}} className='size-44 relative rounded-full' />
             <TouchableOpacity className='absolute bottom-11 right-2' >
               <Image source={icons.edit} className='size-9' />
             </TouchableOpacity>
-            <Text className="text-2xl font-rubik-bold mt-2">Aliaksandr | JSM</Text>
+            <Text className="text-2xl font-rubik-bold mt-2">{user?.name}</Text>
           </View>
         </View>
         <View className="flex flex-col mt-10">
