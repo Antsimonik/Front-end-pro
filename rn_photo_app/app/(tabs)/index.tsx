427d09d5a8dd62e3f9effec5c98f61bd17f3efd4
Alex <antimonikalexandr@gmail.com>
2025-05-22 22:08:59 +0200

    routing

--- rn_photo_app/app/(tabs)/index.tsx
+++ rn_photo_app/app/(tabs)/index.tsx
@@ -1,9 +1,10 @@
+import React from "react";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
-      <Text>Welcome</Text>
+      <Text className="text-5xl text-dark-200 font-bold">Welcome</Text>
     </View>
   );
 }
